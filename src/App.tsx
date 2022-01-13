0a95a4786744a2c2355525269e80e2039c0e1913
Carlose <carlosviniwork@gmail.comm>
2022-01-13 19:42:49 -0300

    Cores dos Botões

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal/index";
 import { GlobalStyle } from "./styles/global";
 
-
+Modal.setAppElement('#root')
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -19,8 +20,11 @@ export function App() {
 
   return (
     <>
-      <Header onOpenNewTransactionsModal={handleOpenNewTransactionModal}/>
+      <Header 
+        onOpenNewTransactionsModal={handleOpenNewTransactionModal}/>
+      
       <Dashboard />
+      
       <NewTransactionModal 
       isOpen={isNewTransactionModalOpen} 
       onRequestClose={handleCloseNewTransactionModal}/>
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from 'react-modal'
-import { Container, TransactionTypeContainer } from './style';
+import { Container, TransactionTypeContainer, RadioBox } from './style';
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { useState } from 'react';
 
 
 interface NewTransactionModalProps {
@@ -12,6 +13,11 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit');
+
+    
+    
+    
     return (
         <Modal 
         isOpen={isOpen}
@@ -32,15 +38,24 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             />
 
             <TransactionTypeContainer> 
-                <button type='button'>
+                <RadioBox 
+                    type='button'
+                    onClick={() => {setType('deposit')}}
+                    isActive = { type == 'deposit'}
+                >
                     <img src={incomeImg} alt="Entrada"/>
                     <span>Entrada</span>
-                </button>
+
+                </RadioBox>
                 
-                <button type='button'>
+                <RadioBox 
+                    type='button'
+                    onClick={() => {setType('withdraw')}}
+                    isActive = { type == 'withdraw'}
+                >
                     <img src={outcomeImg} alt="Saída"/>
                     <span>Saída</span>
-                </button>
+                </RadioBox>
 
             </TransactionTypeContainer>
 
